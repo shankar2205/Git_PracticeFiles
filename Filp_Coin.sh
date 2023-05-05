736e0fdef99ca10f54b937745d0496cab0f6a3d0
“Shankar2205” <shankerb582@gmail.com>
2023-05-05 16:03:44 +0530

    [Shankar]: Changes made in Filp_Coin.sh

--- Filp_Coin.sh
+++ Filp_Coin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -x
 
-a=$((RANDOM%2))
-echo $a
+a=$((RANDOM%2))         # Assigning a variable to Random Value.
+echo $a 
 
-if [ $a -eq 1 ]
+if [ $a -eq 1 ]         # Checking the condition is True of false.
 then
 	echo "Head"
 else
