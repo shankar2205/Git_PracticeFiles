3ce60f28e5746d3a244b80235777d5ad9e913ca7
Shankar <92007719+shankar2205@users.noreply.github.com>
2023-05-05 16:08:33 +0530

    Adding some comments to this file

--- Filp_Coin.sh
+++ Filp_Coin.sh
@@ -5,7 +5,7 @@ echo $a
 
 if [ $a -eq 1 ]         # Checking the condition is True of false.
 then
-	echo "Head"
+	echo "Head"     # If Condition is True It will Print "Head"
 else
-	echo "Tail"
-fi
+	echo "Tail"	# If Condition is False It Will Print "Tails"
+fi			# Closing the Condition by using "fi"
